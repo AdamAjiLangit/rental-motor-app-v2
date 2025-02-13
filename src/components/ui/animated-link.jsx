703b9424d5c3a4fe2be49aaed37289a0d07a9bd9
Adam <105685257+AdamAjiLangit@users.noreply.github.com>
2025-02-13 14:30:21 +0700

    fix : [AD] component-bugged

--- src/components/ui/animated-link.jsx
+++ src/components/ui/animated-link.jsx
@@ -1,69 +1,18 @@
 import React from 'react';
-import styled from 'styled-components';
+import Link from 'next/link';
+import '@/styles/link.css';
 
 const AnimatedLink = ({ href, text }) => {
-    return (
-        <StyledWrapper>
-            <a href={href}>
-                <button className="cta">
-                    <span className="hover-underline-animation">{text}</span>
-                    {/* <svg id="arrow-horizontal" xmlns="http://www.w3.org/2000/svg" width={30} height={10} viewBox="0 0 46 16">
-                        <path id="Path_10" data-name="Path 10" d="M8,0,6.545,1.455l5.506,5.506H-30V9.039H12.052L6.545,14.545,8,16l8-8Z" transform="translate(30)" />
-                    </svg> */}
-                </button>
-            </a>
-        </StyledWrapper>
-    );
+  return (
+    <Link href={href}>
+      <button className="cta">
+        <span className="hover-underline-animation">{text}</span>
+        {/* <svg id="arrow-horizontal" xmlns="http://www.w3.org/2000/svg" width={30} height={10} viewBox="0 0 46 16">
+              <path id="Path_10" data-name="Path 10" d="M8,0,6.545,1.455l5.506,5.506H-30V9.039H12.052L6.545,14.545,8,16l8-8Z" transform="translate(30)" />
+          </svg> */}
+      </button>
+    </Link>
+  );
 }
-
-const StyledWrapper = styled.div`
-  .cta {
-    border: none;
-    background: none;
-    cursor: pointer;
-  }
-
-  .cta span {
-    padding-bottom: 3px;
-    letter-spacing: 2px;
-    font-size: 18px;
-  }
-
-  .cta svg {
-    transform: translateX(-8px);
-    transition: all 0.3s ease;
-  }
-
-  .cta:hover svg {
-    transform: translateX(0);
-  }
-
-  .cta:active svg {
-    transform: scale(0.9);
-  }
-
-  .hover-underline-animation {
-    position: relative;
-    color: #ff4d30;
-    padding-bottom: 20px;
-  }
-
-  .hover-underline-animation:after {
-    content: "";
-    position: absolute;
-    width: 100%;
-    transform: scaleX(0);
-    height: 2px;
-    bottom: 0;
-    left: 0;
-    background-color: #ff4d30;
-    transform-origin: bottom right;
-    transition: transform 0.25s ease-out;
-  }
-
-  .cta:hover .hover-underline-animation:after {
-    transform: scaleX(1);
-    transform-origin: bottom left;
-  }`;
 
 export default AnimatedLink;
