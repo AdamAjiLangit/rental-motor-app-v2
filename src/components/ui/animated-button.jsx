0df76c0114b51383399178858cbd9b2930d34051
Adam <adamajilangit@gmail.com>
2025-02-16 20:21:23 +0700

    feat : [AD] ui-landing (#6)

--- src/components/ui/animated-button.jsx
+++ src/components/ui/animated-button.jsx
@@ -3,20 +3,20 @@
 import Link from 'next/link';
 import React from 'react';
 
-const Button = ({ text, href, onClick }) => {
+const Button = ({ text, href, customStyle }) => {
   return (
-    <div className="button-wrapper">
-      <Link href={href} onClick={onClick} className="button-link">
-        <button className="animated-button">
+    <div className="button-wrapper w-fit">
+      <Link href={href} className="button-link">
+        <div className="animated-button">
           <svg viewBox="0 0 24 24" className="arr-2" xmlns="http://www.w3.org/2000/svg">
             <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z" />
           </svg>
-          <span className="text">{text}</span>
+          <span className={`text ${customStyle}`}>{text}</span>
           <span className="circle"></span>
           <svg viewBox="0 0 24 24" className="arr-1" xmlns="http://www.w3.org/2000/svg">
             <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z" />
           </svg>
-        </button>
+        </div>
       </Link>
     </div>
   );
