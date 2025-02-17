1736bd6559e3fb946a7fd601012050ae9d424f4f
Adam <adamajilangit@gmail.com>
2025-02-17 17:19:15 +0700

    feat : [AD] navbar-footer (#9)
    * feat : [AD] ui-landing
    
    * feat : [AD] ui-review
    
    * feat : [AD] ui-footer-navbar

--- src/components/anim/anim.js
+++ src/components/anim/anim.js
@@ -28,3 +28,78 @@ export const opacity = {
         transition: { duration: 0.5 }
     }
 }
+
+export const preloaderOpacity = {
+    initial: {
+        opacity: 0
+    },
+    enter: {
+        opacity: 0.75,
+        transition: { duration: 1, delay: 0.2 }
+    },
+}
+
+export const preloaderSlideUp = {
+    initial: {
+        top: 0
+    },
+    exit: {
+        top: "-100vh",
+        transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1], delay: 0.2 }
+    }
+}
+
+export const heroSlideUp = {
+    initial: {
+        y: 0
+    },
+    enter: {
+        y: 0,
+        transition: { duration: 0.6, ease: [0.33, 1, 0.68, 1], delay: 2.5 }
+    }
+}
+
+export const descriptionSlideUp = {
+    initial: {
+        y: "100%"
+    },
+    open: (i) => ({
+        y: "0%",
+        transition: { duration: 0.5, delay: 0.01 * i }
+    }),
+    closed: {
+        y: "100%",
+        transition: { duration: 0.5 }
+    }
+}
+
+export const descriptionOpacity = {
+    initial: {
+        opacity: 0
+    },
+    open: {
+        opacity: 1,
+        transition: { duration: 0.5 }
+    },
+    closed: {
+        opacity: 0,
+        transition: { duration: 0.5 }
+    }
+}
+
+export const linkMenuSlide = {
+    initial: { x: "calc(100% + 100px)" },
+    enter: { x: "0", transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1] } },
+    exit: { x: "calc(100% + 100px)", transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1] } }
+}
+
+export const linkSlide = {
+    initial: { x: 80 },
+    enter: i => ({ x: 0, transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1], delay: 0.05 * i } }),
+    exit: i => ({ x: 80, transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1], delay: 0.05 * i } })
+}
+
+export const linkScale = {
+    open: { scale: 1, transition: { duration: 0.3 } },
+    closed: { scale: 0, transition: { duration: 0.4 } }
+}
---
+++ src/components/layouts/Navbar/Nav/NavItems.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react'
+import { motion } from 'framer-motion';
+import { usePathname } from 'next/navigation';
+import { linkMenuSlide } from '@/components/anim/anim';
+import LinkNavbar from './Link';
+import Curve from './Curve';
+
+const navItems = [
+    {
+        title: "Home",
+        href: "/",
+    },
+    {
+        title: "Your Order",
+        href: "/order",
+    },
+    {
+        title: "Catalog",
+        href: "/catalog",
+    },
+    {
+        title: "Contact",
+        href: "/contact",
+    },
+    {
+        title: "About",
+        href: "/About",
+    },
+]
+
+export default function NavItems() {
+    const pathname = usePathname();
+    const [selectedIndicator, setSelectedIndicator] = useState(pathname);
+
+    return (
+        <motion.div
+            variants={linkMenuSlide}
+            initial="initial"
+            animate="enter"
+            exit="exit"
+            className="h-screen bg-[rgb(41,41,41)] fixed right-0 top-0 text-white z-30"
+        >
+            <div className="box-border p-[100px] flex flex-col gap-5 justify-between">
+                <div
+                    onMouseLeave={() => {
+                        setSelectedIndicator(pathname);
+                    }}
+                    className="flex flex-col text-[56px] gap-3 mt-5"
+                >
+                    <div className="text-[rgb(153,153,153)] border-b border-[rgb(153,153,153)] uppercase text-[11px] mb-10">
+                        <p>Navigation</p>
+                    </div>
+                    {navItems.map((data, index) => (
+                        <LinkNavbar
+                            key={index}
+                            data={{ ...data, index }}
+                            isActive={selectedIndicator == data.href}
+                            setSelectedIndicator={setSelectedIndicator}
+                        />
+                    ))}
+                </div>
+            </div>
+            <Curve />
+        </motion.div>
+    )
+}
