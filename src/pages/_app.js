2f05a95bea735aedc1026d5374662b8dbefeed70
Adam <adamajilangit@gmail.com>
2025-02-23 18:29:04 +0700

    Vercel (#16)
    * feat : [AD] ui-landing
    
    * feat : [AD] ui-review
    
    * feat : [AD] ui-footer-navbar
    
    * feat : [AD] ui-404
    
    * feat : [AD] ui-review-padding
    
    * feat : [AD] ui-catalog
    
    * feat : [AD] ui-about
    
    * feat : [AD] ui-contact
    
    * feat : [AD] ui-login-register
    
    * vercel-analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import "@/styles/styles.css";
 import { AnimatePresence } from 'framer-motion';
+import { Analytics } from "@vercel/analytics/react"
 // import Layout from "@/components/Layout";
 
 export default function App({ Component, pageProps, router }) {
@@ -10,6 +11,7 @@ export default function App({ Component, pageProps, router }) {
         <AnimatePresence mode='wait'>
           <Component key={router.route} {...pageProps} />
         </AnimatePresence>
+        <Analytics />
       </div>
     </>
   );
