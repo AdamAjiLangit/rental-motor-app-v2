442fb21db66bfec67aa6d3701dc8976e69435998
Adam <105685257+AdamAjiLangit@users.noreply.github.com>
2025-02-23 17:52:22 +0700

    feat : [AD] ui-login-register

--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -4,23 +4,25 @@ import React from 'react';
 import dynamic from 'next/dynamic';
 import { ReactLenis } from "lenis/react";
 
-const ContactComponent = dynamic(() => import('@/components/pages/Contact/ContactComponent'));
+const RegisterComponent = dynamic(() => import('@/components/pages/Register/RegisterComponent'), { ssr: false });
 const Navbar = dynamic(() => import('@/components/layouts/Navbar/Navbar'));
 const Footer = dynamic(() => import('@/components/layouts/Footer/Footer'));
 
 import Curve from '@/components/layouts/Curve/Curve';
 import Button from '@/components/ui/animated-button';
 
-const Kontak = () => {
+const Register = () => {
     return (
         <ReactLenis root options={{ lerp: 0.1, duration: 1.5 }}>
             <Curve>
-                <Navbar />
-                <ContactComponent />
-                <Footer />
+                {/* <Navbar /> */}
+                <div className='flex justify-center items-center h-screen'>
+                    <RegisterComponent />
+                </div>
+                {/* <Footer /> */}
             </Curve>
         </ReactLenis>
     )
 }
 
-export default Kontak
+export default Register
