48eca4421d6b93fe80d26ecdcabd73cf1959b99b
Adam <adamajilangit@gmail.com>
2025-04-24 10:02:16 +0700

    Seo (#40)
    * feat : [AD] ui-landing
    
    * feat : [AD] ui-review
    
    * feat : [AD] ui-footer-navbar
    
    * feat : [AD] ui-404
    
    * feat : [AD] ui-review-padding
    
    * feat : [AD] ui-catalog
    
    * feat : [AD] ui-about
    
    * feat : [AD] ui-contact
    
    * feat : [AD] ui-login-register
    
    * feat : [AD] ui-navbar
    
    * feat : [AD] auth-login-middleware
    
    * feat : [AD] auth-register
    
    * feat : [AD] auth-loading
    
    * fix : [AD] performance
    
    * fix : [AD] performance
    
    * fix : [AD] performance
    
    * develop : [AD]
    
    * feat : [AD] landing-api
    
    * feat : [AD] catalog-api
    
    * feat : [AD] api-detail-motor
    
    * seo : [AD]
    
    * seo : [AD]
    
    * seo : [AD]
    
    * seo : [AD]
    
    * seo : [AD]
    
    * seo
    
    * seo
    
    * seo

--- next-sitemap.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: 'https://rentalmotorkudus.vercel.app', // your site URL
-    generateRobotsTxt: true, // also generates robots.txt
-    changefreq: 'weekly', // default change frequency
-    priority: 0.7, // default priority
-    sitemapSize: 7000, // default max URLs per sitemap file
-};
